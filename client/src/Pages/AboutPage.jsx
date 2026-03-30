006e7ddc812e1c034388cd23ee853a31ae65a79e
Elijah <kamokane06@gmail.com>
2026-03-29 21:58:55 -0400

    Renamed .JSX pages for import

---
+++ client/src/Pages/AboutPage.jsx
@@ -0,0 +1,20 @@
+import React, { useEffect, useMemo, useState } from "react";
+
+
+  return (
+    <>
+      <div class = "header">
+        <section>
+            <div class = "opening-hours"> 
+                <p>Opening Hours</p>
+                <p>Monday - Friday: 10am - 8pm</p>
+            </div>
+
+        </section>
+
+
+      </div>
+
+      
+    </>
+  );
