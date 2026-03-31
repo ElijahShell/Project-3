8e595158aa1bc2c9b7dfde7e8f887e9c0a45009b
Elijah <kamokane06@gmail.com>
2026-03-31 19:09:50 -0400

    Adjusted Navbar and Added code to hero

--- client/src/components/Hero.jsx
+++ client/src/components/Hero.jsx
@@ -5,10 +5,10 @@ export default function Hero() {
         <header className="hero">
             <div className="overlay"></div>
             <div className="hero-content">
-                <h1>
-                   
-                </h1>
+                <h1>Wolfe's Tattoo Gallery</h1>
+                <p>Discover the art of ink at Wolfe's Tattoo Gallery. Our talented artists bring your vision to life with precision and creativity. From intricate designs to bold statements, we offer a wide range of styles to suit every taste. Book your appointment today and let us turn your ideas into stunning tattoos that you'll cherish forever.</p>
             </div>
         </header>
-    )
+
+    );
 }
--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -5,6 +5,8 @@ export default function Navbar() {
         <nav className="navbar">
             <h2 className="logo">Wolfe's Tattoo Gallery</h2>
         
+            <p>123 TATTOO LANE, KNOXVILLE, TN, 37909</p>
+
             <ul className="nav-links">
                 <li><a href="#">Home</a></li>
                 <li><a href="#">About</a></li>
