5161d83fee375cbca2d2810d3b6c4b78445f657f
Elijah <kamokane06@gmail.com>
2026-04-02 10:59:47 -0400

    Moved to Pages folder, updated name.

---
+++ client/src/Pages/HeroPage.jsx
@@ -0,0 +1,14 @@
+import "../styles/hero.css";
+
+export default function Hero() {
+    return(
+        <header className="hero">
+            <div className="overlay"></div>
+            <div className="hero-content">
+                <h1>Wolfe's Tattoo Gallery</h1>
+                <p>Discover the art of ink at Wolfe's Tattoo Gallery. Our talented artists bring your vision to life with precision and creativity. From intricate designs to bold statements, we offer a wide range of styles to suit every taste. Book your appointment today and let us turn your ideas into stunning tattoos that you'll cherish forever.</p>
+            </div>
+        </header>
+
+    );
+}
